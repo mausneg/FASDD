edf013efd6b38e295f45e7a733a82be31d83144b
mausneg <maulanasuryanegara123@gmail.com>
2024-05-07 23:21:29 +0800

    add bottom navbar

---
+++ app/src/main/java/com/example/fasdd_android/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.fasdd_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fasdd_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_scan -> replaceFragment(ScanFragment())
+                R.id.nav_settings -> replaceFragment(SettingsFragment())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.commit()
+    }
+}
