f77ad6d60aa695ec01d095231eafdb285bb1b48b
destiasuhada1 <destiasuhada96@gmail.com>
2024-05-07 14:09:45 +0800

    First Show Activity dan Login UI

---
+++ app/src/main/java/com/example/fasdd_android/FstShow.kt
@@ -0,0 +1,11 @@
+package com.example.fasdd_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FstShow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fstshow)
+    }
+}
