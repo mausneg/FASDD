6a04c140c2f3b6c0b6d7729b56ba9088bdcb719c
destiasuhada1 <destiasuhada96@gmail.com>
2024-05-13 12:43:58 +0800

    Fix Destia 5

--- app/src/main/java/com/example/fasdd_android/FstShowActivity.kt
+++ app/src/main/java/com/example/fasdd_android/FstShowActivity.kt
@@ -3,14 +3,14 @@ package com.example.fasdd_android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fasdd_android.databinding.ActivitySplashscreenBinding
+import com.example.fasdd_android.databinding.ActivityFstshowBinding
 
 class FstShowActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashscreenBinding
+    private lateinit var binding: ActivityFstshowBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        binding = ActivityFstshowBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.logofasdd.alpha = 0f
