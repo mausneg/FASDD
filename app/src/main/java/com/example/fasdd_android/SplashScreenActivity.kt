2a4034aee7d434bf7c596f960e2415d99c9138b5
destiasuhada1 <destiasuhada96@gmail.com>
2024-05-13 12:58:55 +0800

    Fix Destia JAdi

--- app/src/main/java/com/example/fasdd_android/SplashScreenActivity.kt
+++ app/src/main/java/com/example/fasdd_android/SplashScreenActivity.kt
@@ -3,13 +3,13 @@ package com.example.fasdd_android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fasdd_android.databinding.ActivityFstshowBinding
+import com.example.fasdd_android.databinding.ActivitySplashscreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityFstshowBinding
+    private lateinit var binding: ActivitySplashscreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityFstshowBinding.inflate(layoutInflater)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.logofasdd.alpha = 0f
         binding.logofasdd.animate().setDuration(1500).alpha(1f).withEndAction {
